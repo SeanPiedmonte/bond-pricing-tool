e90967d078afffdbacee0eb3ff0c06008ac86756
SeanPiedmonte <seanpiedmonte@gmail.com>
2025-11-09 05:26:37 -0600

    Create a Money class to handle different transactions

---
+++ Money.h
@@ -0,0 +1,95 @@
+#include <exception>
+#include <iostream>
+#include <string>
+
+class Money {
+    private:
+        // 6 decimals of precision units. E.g: 12.45 = 12450000
+        long long units;
+
+        void swap(Money other);
+
+    public:
+        Money(const Money &other);
+        Money(const Money &&other);
+        Money(long long _units);
+        ~Money();
+
+        Money& operator=(const Money &other) {
+            if (&other != this) {
+                units = other.units;
+            }
+            return *this;
+        }
+
+        bool operator==(const Money &other) const {
+            return other.units == units; 
+        }
+
+        bool operator!=(const Money &other) const {
+            return !(*this == other);
+        }
+
+        bool operator>=(const Money &other) const {
+            return other.units >= units;
+        }
+
+        bool operator<=(const Money &other) const {
+            return other.units <= units;
+        }
+
+        bool operator>(const Money &other) const {
+            return other.units > units; 
+        }
+
+        bool operator<(const Money &other) const {
+            return other.units < units;
+        }
+
+        Money operator+(const Money &other) const {
+            Money result(0);
+            long long res_units = units + other.units;
+            result.units = res_units;
+            return result;  
+        }
+
+        Money operator-(const Money &other) const {
+            Money result(0);
+            long long res_units = units - other.units;
+            result.units = res_units;
+            return result;
+        }
+
+        Money operator*(const Money &other) const {
+            Money result(0);
+            long long res_units = other.units * units;
+            result.units = res_units;
+            return result;
+        }
+
+        Money operator/(const Money &other) const {
+            Money result(0);
+            long long rem = units;
+            long long quo = 0;
+            if (other.units > units) {
+                result.units = 0;
+                return result;
+            }
+
+            if (units == other.units) {
+                result.units = 1;
+                return result;
+            }
+            
+            while (other.units <= units) {
+                rem -= other.units;
+                quo++;
+            }
+
+            result.units = quo;
+            return result;
+        }
+        
+        
+        friend std::ostream& operator<<(std::ostream& os, const Money& money); 
+};
